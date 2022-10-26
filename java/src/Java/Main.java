51a15b4a6c42df51184b9b69994537d8f020a518
joshuajonghankim <joshuajonghankim@gmail.com>
2022-10-26 05:45:38 -0400

    java

--- java/src/Java/Main.java
+++ java/src/Java/Main.java
@@ -3,6 +3,21 @@ package Java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int level;
+        level = 10;
+        System.out.println(level);
+
+        int a = 10;
+
+        long num = 12345678900L; //large numbers out of range (-2^63 ~ 2^63 - 1)
+        long num2 = 1000;
+
+        char ch1 = '한';
+        char ch2 = '\uD55C';
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+
     }
 }
